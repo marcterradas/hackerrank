f736104b19540276b3e1de2ca31d8dda47553f2b
marcterradas <terradas.marc@gmail.com>
2022-04-24 19:06:49 +0200

    plus minus exercice

---
+++ algorithms/plus-minus.test.js
@@ -0,0 +1,27 @@
+
+function plusMinus( array ){
+    const numberElements = array.length;
+
+    let positiveNumber = 0
+    let negativeNumber = 0
+    let zeroNumber = 0
+
+    for(let index = 0; index < numberElements; index++){
+        if(array[index] > 0) positiveNumber++
+        else if(array[index] < 0) negativeNumber++
+        else zeroNumber++
+    }
+
+    const positivePercentage = (positiveNumber / numberElements).toFixed(6)
+    const negativePercentage = (negativeNumber / numberElements).toFixed(6)
+    const zeroPercentage = (zeroNumber / numberElements).toFixed(6)
+    const result = [positivePercentage, negativePercentage, zeroPercentage]
+
+    return result
+}
+
+
+test('plus-minus',function(){
+    const array = [-4, 3, -9, 0, 4, 1]
+    expect(plusMinus(array)).toEqual([ '0.500000', '0.333333', '0.166667' ])
+})
