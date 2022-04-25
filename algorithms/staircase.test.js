6129c6a9e22562b79d8cd0fa051ab3faa8df378f
marcterradas <terradas.marc@gmail.com>
2022-04-25 20:21:23 +0200

    staircase

---
+++ algorithms/staircase.test.js
@@ -0,0 +1,24 @@
+
+function staircase(stairCaseLength) {
+
+    let arrResult = []
+    
+    for( let index = 1; index <= stairCaseLength; index++ ){
+
+        let newElement = ''
+        let numberOfSpaces = stairCaseLength - index
+
+        newElement = newElement.padStart(numberOfSpaces,' ')
+        newElement = newElement.padEnd(stairCaseLength,'#')
+
+        arrResult[index - 1] = newElement
+
+    }
+
+    return arrResult
+
+}
+
+test('staircase',function(){
+    expect(staircase(6)).toEqual([ '     #', '    ##', '   ###', '  ####', ' #####', '######' ])
+})
